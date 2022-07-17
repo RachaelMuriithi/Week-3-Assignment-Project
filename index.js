85091730a23943066dc46691867472b2fb2527d9
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-07-17 09:12:46 +0300

    my function properties

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ let shedC = [ "cowI", "cowJ", "cowK", "cowL" ];
 let shedD = [ "cowM", "cowN", "cowO", "cowP" ];
 let totalProduction = shedA.concat (shedB, shedC, shedD);
 
-let name = input ("enter the name of the cow i.e cowA");
-let milk = input ("milk the cow produces per day in litres")
+let milkforcowA = input ("milk cowA produced that day in litres");
+let milkforcowB = input ("milk cowB produced that day in litres");
+let milkforcowC = input ( "milk cowC produced that day in litres");
+let milkforcowD = input ( "milk cowD produced that day in litres");
+let milkforcowE = input ( "milk cowE produced that day in litres");
+let milkforcowF = input ( "milk cowF produced that day in litres");
+let milkforcowG = input ( "milk cowG produced that day in litres");
+let milkforcowH = input ( "milk cowH produced that day in litres");
+let milkforcowI = input ( "milk cowI produced that day in litres");
+let milkforcowJ = input ( "milk cowJ produced that day in litres");
+let milkforcowK = input ( "milk cowK produced that day in litres");
+let milkforcowL = input ( "milk cowL produced that day in litres");
+let milkforcowM = input ( "milk cowM produced that day in litres");
+let milkforcowN = input ( "milk cowN produced that day in litres");
+let milkforcowO = input ( "milk cowO produced that day in litres");
+let milkforcowP = input ( "milk cowP produced that day in litres");
+function milkProductionPerDay () {
+    
+    var milkPerDayForShedA = (milkforcowA + milkforcowB + milkforcowC + milkforcowD);
+    var milkPerDayForShedB = (milkforcowE + milkforcowF + milkforcowG + milkforcowH);
+    var milkPerDayForShedC = (milkforcowI + milkforcowJ + milkforcowK +milkforcowL);
+    var milkPerDayForShedD = (milkforcowM + milkforcowN + milkforcowO + milkforcowP);
+    var totalMilkPPerDay = (milkPerDayForShedA + milkPerDayForShedB + milkPerDayForShedC
+                            + milkPerDayForShedD);
+}
 
 
 
