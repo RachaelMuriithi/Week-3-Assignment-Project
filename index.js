36b068621dabc980081eecf561959a16d6f9fad0
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-07-17 08:39:19 +0300

    Store my variables for the first function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let shedA = [ "cowA", "cowB", "cowC", "cowD" ];
+let shedB = [ "cowE", "cowF", "cowG", "cowH" ];
+let shedC = [ "cowI", "cowJ", "cowK", "cowL" ];
+let shedD = [ "cowM", "cowN", "cowO", "cowP" ];
+let totalProduction = shedA.concat (shedB, shedC, shedD);
+
+let name = input ("enter the name of the cow i.e cowA");
+let milk = input ("milk the cow produces per day in litres")
 
 
 
